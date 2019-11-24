166507043f5867e70f477f8810d6cc396fd00133
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-24 15:28:56 +0100

    Disable R script for now due to install issues.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo apt update
+#sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/'
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+#sudo apt update
 
 # Install R and R packages
-echo Install R and R packages
+#echo Install R and R packages
 
-sudo apt-get install -y r-base-core=3.5.2-1xenial0
-sudo Rscript -e "install.packages(c(\"missMDA\", \"readr\", \"forcats\"), repos = \"http://cran.us.r-project.org\")"
-sudo Rscript ./script.R
+#sudo apt-get install -y r-base-core=3.5.2-1xenial0
+#sudo Rscript -e "install.packages(c(\"missMDA\", \"readr\", \"forcats\"), repos = \"http://cran.us.r-project.org\")"
+#sudo Rscript ./script.R
 
 # Install Python packages
 echo Install Python packages
