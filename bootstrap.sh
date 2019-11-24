f53cce6c8f3c80c1e727425440c8f076de8fb567
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-24 14:45:18 +0100

    Fix typo.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-sudo add-apt-repository 'https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/'
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo apt-get update
+sudo apt update
 
 # Install R and R packages
 echo Install R and R packages
