7263161d68d8a79301462faa311abadb5ee032ea
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-24 13:18:18 +0100

    Update boostrap to fix tensorflow install issues.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,11 @@ sudo apt-get update
 
 sudo apt-get install -y r-base
 sudo Rscript -e "install.packages(c(\"missMDA\", \"tidyverse\"))"
+sudo Rscript ./script.R
 
 # Install Python packages
 pip install --upgrade pip
+pip install numpy
+pip install setuptools
+pip install --upgrade numpy setuptools # Causing issues to install tensorflow
 pip install -r ./packages_py.txt
