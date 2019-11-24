33f8e955c921d3d3aee84b73cbb2a6d838a71ea2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-24 14:39:34 +0000

    Deploy clementviolet/Projet_MLB to github.com/clementviolet/Projet_MLB.git:gh-pages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,21 @@
 #! /bin/bash
 
-sudo apt-get update
+#sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/'
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+#sudo apt update
 
 # Install R and R packages
+#echo Install R and R packages
 
-sudo apt-get install -y r-base
-sudo Rscript -e "install.packages(c("missMDA", "tidyverse"))"
+#sudo apt-get install -y r-base-core=3.5.2-1xenial0
+#sudo Rscript -e "install.packages(c(\"missMDA\", \"readr\", \"forcats\"), repos = \"http://cran.us.r-project.org\")"
+#sudo Rscript ./script.R
 
 # Install Python packages
+echo Install Python packages
+
 pip install --upgrade pip
+pip install numpy
+pip install setuptools
+pip install --upgrade numpy setuptools # Causing issues to install tensorflow
 pip install -r ./packages_py.txt
