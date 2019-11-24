2b9ea5638d031833aad7d69d0cc24d734c329920
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-24 13:42:23 +0100

    Add echo messages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,15 @@
 sudo apt-get update
 
 # Install R and R packages
+echo Install R and R packages
 
 sudo apt-get install -y r-base
 sudo Rscript -e "install.packages(c(\"missMDA\", \"tidyverse\"), repos = \"http://cran.us.r-project.org\")"
 sudo Rscript ./script.R
 
 # Install Python packages
+echo Install Python packages
+
 pip install --upgrade pip
 pip install numpy
 pip install setuptools
