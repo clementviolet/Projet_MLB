e0dd3970ec8e98e08e104ef4a15f37835684419e
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-24 13:30:58 +0100

    Select CRAN repo for R packages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 # Install R and R packages
 
 sudo apt-get install -y r-base
-sudo Rscript -e "install.packages(c(\"missMDA\", \"tidyverse\"))"
+sudo Rscript -e "install.packages(c(\"missMDA\", \"tidyverse\"), repos = \"http://cran.us.r-project.org\")"
 sudo Rscript ./script.R
 
 # Install Python packages
