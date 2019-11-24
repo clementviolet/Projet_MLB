1d3598da516869e016ba7b6dc4c4444df84aeb8e
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-24 12:34:53 +0100

    Fix typo with quotes R packages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 # Install R and R packages
 
 sudo apt-get install -y r-base
-sudo Rscript -e "install.packages(c("missMDA", "tidyverse"))"
+sudo Rscript -e "install.packages(c(\"missMDA\", \"tidyverse\"))"
 
 # Install Python packages
 pip install --upgrade pip
